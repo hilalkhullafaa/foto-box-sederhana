e1a3077fe794c64de30579be4c9e7e481f6bacd5
hilalkhullafaa <hilalzeppelin@gmail.com>
2024-07-16 13:52:53 +0700

    melakukan perbaikan pada bug jumbo elemen ketika class active

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const container = document.querySelector(".container");
 const jumbo = document.querySelector(".jumbo");
-const thumbs = document.querySelectorAll(".thumb");
-
+// const thumbs = document.querySelectorAll(".thumb");
+let eOld = null;
 container.addEventListener("click", function (e) {
   // cek apakah yang di-klik adalah thumb
-  if (e.target.className == "thumb") jumbo.src = e.target.src;
-  jumbo.classList.add("fade");
-  setTimeout(() => {
-    jumbo.classList.remove("fade");
-  }, 500);
+  //   if (e.target.className == "thumb") jumbo.src = e.target.src;
+  //   jumbo.classList.add("fade");
+  //   setTimeout(() => {
+  //     jumbo.classList.remove("fade");
+  //   }, 500);
+  //   thumbs.forEach((thumb) => {
+  //     thumb.className = "thumb";
+  //   });
+  //   e.target.classList.add("active");
 
-  thumbs.forEach((thumb) => {
-    thumb.className = "thumb";
-  });
-
-  e.target.classList.add("active");
+  if (e.target.className == "thumb") {
+    if (eOld != null) {
+      eOld.classList.remove("active");
+    }
+    jumbo.src = e.target.src;
+    jumbo.classList.add("fade");
+    setTimeout(function () {
+      jumbo.classList.remove("fade");
+    }, 500);
+    e.target.classList.add("active");
+    eOld = e.target;
+  }
 });
